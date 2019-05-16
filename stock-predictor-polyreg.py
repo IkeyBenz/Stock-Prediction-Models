22b1193dfc10687e26051724c0db769736820e95
Ikey Benzaken <ikey.benz@gmail.com>
2019-05-16 15:36:55 -0700

    Final additions

--- stock-predictor-polyreg.py
+++ stock-predictor-polyreg.py
@@ -5,35 +5,38 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from datetime import timedelta
 
-df = get_stock("AAPL")[::-1]
 
-X = np.array(range(len(df)))
-y = df.close
+def predict_stock_polyreg(symbol: str):
 
-a, b, c, d = np.polyfit(X, y, 3)
+    df = get_stock(symbol)
+
+    X = np.array(range(len(df)))
+    y = df.close
+
+    a, b, c, d = np.polyfit(X, y, 3)
+
+    def f(x):
+        return a*x**3 + b*x**2 + c*x + d
+
+    plt.plot(df.index, y)  # Actual
+    plt.plot(df.index, [f(x) for x in X])  # Regression model's interetation
+
+    def predict_x_days_future(days: int):
+        X_future = np.array(range(len(df), len(df) + days))
+        y_future = [f(x) for x in X_future]
+
+        X_future_dates = [df.index[-1] +
+                          timedelta(days=i) for i in range(days)]
+
+        plt.plot(X_future_dates, y_future)
+
+        # The predicted price for this many days in the future
+        return y_future[-1]
+
+    predict_x_days_future(365)
+    plt.title(symbol)
+    plt.show()
 
 
-def f(x):
-    return a*x**3 + b*x**2 + c*x + d
-
-
-plt.plot(df.index, y)  # Actual
-plt.plot(df.index, [f(x) for x in X])  # Regression model's interetation
-
-
-def predict_x_days_future(days: int):
-    X_future = np.array(range(len(df), len(df) + days))
-    y_future = [f(x) for x in X_future]
-
-    X_future_dates = [df.index[-1] + timedelta(days=i) for i in range(days)]
-
-    plt.plot(X_future_dates, y_future)
-
-    return y_future[-1]  # The predicted price for this many days in the future
-
-
-price_next_year = predict_x_days_future(365)
-plt.plot()
-plt.show()
-
-print(price_next_year)
+for symbol in ["MSFT", "NFLX", "AAPL", "TSLA"]:
+    predict_stock_polyreg(symbol)
--- stock_predictor-linreg.py
+++ stock_predictor-linreg.py
@@ -5,35 +5,43 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-# Instantiate Data Frame:
-df = get_stock("MSFT")[::-1]
-# Declare X and y values for LinReg:
-X = np.array(range(len(df)))
-y = df[["close"]]
 
-# Split data into training and testing sets:
-X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=False)
+def linreg_predict_stock(symbol: str):
 
-X_train = np.array(X_train).reshape(-1, 1)  # These three 1d arrays need
-X_test = np.array(X_test).reshape(-1, 1)    # to be 2d arrays. So we use
-#                                             reshape() to convert them.
+    # Instantiate Data Frame:
+    df = get_stock(symbol)
+    # Declare X and y values for LinReg:
+    X = np.array(range(len(df)))
+    y = df[["close"]]
 
-# Instantiate the LinReg model:
-model = LinearRegression(     # Don't calc y-intercept because we
-    fit_intercept=False,      # know it is the first item in X.
-)
+    # Split data into training and testing sets:
+    X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=False)
 
-# Fit model using training data:
-model.fit(X_train, y_train)
+    X_train = np.array(X_train).reshape(-1, 1)  # These three 1d arrays need
+    X_test = np.array(X_test).reshape(-1, 1)    # to be 2d arrays. So we use
+    #                                             reshape() to convert them.
 
-# Test model for accuracy
-y_pred = model.predict(X_test)
+    # Instantiate the LinReg model:
+    model = LinearRegression(     # Don't calc y-intercept because we
+        fit_intercept=False,      # know it is the first item in X.
+    )
 
-model.score(y_pred, y_test)
-model.coef_
+    # Fit model using training data:
+    model.fit(X_train, y_train)
 
-# Visualize prediction
-plt.plot(df.index[:len(X_train)], y_train)
-plt.plot(df.index[len(X_train):], y_test)
-plt.plot(df.index[len(X_train):], y_pred)
-plt.show()  # Pretty aweful
+    # Test model for accuracy
+    y_pred = model.predict(X_test)
+
+    print(f"{symbol} linreg model score: {model.score(y_pred, y_test)}")
+    print(f"{symbol} linreg model coeficient: {model.coef_}")
+
+    # Visualize prediction
+    plt.plot(df.index[:len(X_train)], y_train)
+    plt.plot(df.index[len(X_train):], y_test)
+    plt.plot(df.index[len(X_train):], y_pred)
+    plt.title(symbol)
+    plt.show()  # Pretty aweful
+
+
+for symbol in ["MSFT", "NFLX", "AAPL", "TSLA"]:
+    linreg_predict_stock(symbol)
